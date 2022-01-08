ac9a380cb0f441903960da97911a55d67f3001e8
Yash Kadam <yashkadam278@gmail.com>
2022-01-08 13:15:49 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        merry: ["Merriweather"],
+      },
+    },
+  },
+  plugins: [],
+};
